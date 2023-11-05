bf4fdb4712e7719cb71cedf42ae0102aa600f89a
Haris Kurspahic <haris.kurspahic@moonshiner.at>
2023-11-05 19:18:45 +0100

    REST test

--- src/main/java/com/kursph/SpringBootExampleApplication.java
+++ src/main/java/com/kursph/SpringBootExampleApplication.java
@@ -2,7 +2,10 @@ package com.kursph;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootExampleApplication {
 
@@ -10,4 +13,8 @@ public class SpringBootExampleApplication {
 		SpringApplication.run(SpringBootExampleApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String greet() {
+		return "Hello";
+	}
 }
