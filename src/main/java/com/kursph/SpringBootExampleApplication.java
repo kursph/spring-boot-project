c372f0ea0b1ff2a760a2a818a134c55b0c7fcc58
Haris Kurspahic <haris.kurspahic@moonshiner.at>
2023-11-05 19:47:11 +0100

    JSON for Java

--- src/main/java/com/kursph/SpringBootExampleApplication.java
+++ src/main/java/com/kursph/SpringBootExampleApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @SpringBootApplication
 public class SpringBootExampleApplication {
@@ -14,7 +16,20 @@ public class SpringBootExampleApplication {
 	}
 
 	@GetMapping("/")
-	public String greet() {
-		return "Hello";
+	public GreetResponse greet() {
+		return new GreetResponse(
+				"Hello",
+				List.of("Java", "PHP"),
+				new Person("Alex", 28, 30_000)
+		);
+	}
+
+	record Person(String name, int age, double savings) {}
+
+	record GreetResponse(
+			String response,
+			List<String> favProgrammingLanguages,
+			Person person
+	) {
 	}
 }
