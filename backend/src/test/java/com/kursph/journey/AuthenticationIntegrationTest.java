d936e95f8a59349fc929d543a0718d5873dce91d
Haris Kurspahic <haris.kurspahic@moonshiner.at>
2023-11-17 13:47:38 +0100

    added authentication integration test

---
+++ backend/src/test/java/com/kursph/journey/AuthenticationIntegrationTest.java
@@ -0,0 +1,94 @@
+package com.kursph.journey;
+
+import com.kursph.auth.AuthenticationRequest;
+import com.kursph.auth.AuthenticationResponse;
+import com.kursph.customer.CustomerDTO;
+import com.kursph.customer.CustomerRegistrationRequest;
+import com.kursph.jwt.JWTUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AuthenticationIntegrationTest {
+    private static final String CUSTOMER_URI = "/api/v1/customers";
+    private static final String AUTHENTICATION_URI = "/api/v1/auth/login";
+    @Autowired
+    private WebTestClient webTestClient;
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @Test
+    void canLogin() {
+        String email = "alex@gmail.com";
+        CustomerRegistrationRequest customerRegistrationRequest = new CustomerRegistrationRequest(
+                "Alex",
+                email,
+                "foobar", 20,
+                "MALE"
+        );
+
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest(
+                email,
+                "foobar"
+        );
+
+        webTestClient.post()
+                .uri(AUTHENTICATION_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(authenticationRequest), AuthenticationRequest.class)
+                .exchange()
+                .expectStatus()
+                .isForbidden();
+
+        String jwt = webTestClient.post()
+                .uri(CUSTOMER_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(customerRegistrationRequest), CustomerRegistrationRequest.class)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .returnResult(Void.class)
+                .getResponseHeaders()
+                .get(HttpHeaders.AUTHORIZATION)
+                .get(0);
+
+        EntityExchangeResult<AuthenticationResponse> result = webTestClient.post()
+                .uri(AUTHENTICATION_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(authenticationRequest), AuthenticationRequest.class)
+                .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", jwt))
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(new ParameterizedTypeReference<AuthenticationResponse>() {
+                })
+                .returnResult();
+
+        AuthenticationResponse authenticationResponse = result.getResponseBody();
+
+        assert authenticationResponse != null;
+        CustomerDTO customerDTO = authenticationResponse.customerDTO();
+        assertThat(jwtUtil.isTokenValid(jwt, customerDTO.username())).isTrue();
+
+        assertThat(customerDTO.email()).isEqualTo(email);
+        assertThat(customerDTO.age()).isEqualTo(20);
+        assertThat(customerDTO.name()).isEqualTo("Alex");
+        assertThat(customerDTO.username()).isEqualTo(email);
+        assertThat(customerDTO.gender()).isEqualTo("MALE");
+        assertThat(customerDTO.roles()).isEqualTo(List.of("ROLE_USER"));
+    }
+}
