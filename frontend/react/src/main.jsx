a87f19d6e2405391c7cbd13b6c2bd1d811b163db
Haris Kurspahic <haris.kurspahic@moonshiner.at>
2023-11-17 23:48:50 +0100

    configure router test

--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {ChakraProvider} from "@chakra-ui/react";
 import {createStandaloneToast} from "@chakra-ui/toast";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const {ToastContainer} = createStandaloneToast();
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>test</h1>
+    },
+    {
+        path: "dashboard",
+        element: <App />
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ChakraProvider>
-            <App/>
+            <RouterProvider router={router} />
             <ToastContainer />
         </ChakraProvider>
     </React.StrictMode>,
