1cfe175ca55d1cf722e33112466c0b6f52a60119
Haris Kurspahic <haris.kurspahic@moonshiner.at>
2023-11-20 14:58:32 +0100

    added actuator config and config for new environment

--- backend/src/main/java/com/kursph/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/kursph/security/SecurityFilterChainConfig.java
@@ -43,6 +43,8 @@ public class SecurityFilterChainConfig {
                         "/api/v1/auth/login"
                 )
                 .permitAll()
+                .requestMatchers(HttpMethod.GET, "/actuator/**")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
